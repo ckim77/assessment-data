b0fcbd0a3ac4eabf4867ea1e6453d00ca122213e
Chris Kim <ckjr92@gmail.com>
2021-11-12 16:08:21 -0700

    made some review changes

--- process.py
+++ process.py
@@ -6,7 +6,19 @@ def sales_reports(log_file): # def is the boilerplate code needed to code a func
         line = line.rstrip() # rstrip is a method that removes white space, which is a very handy method to clean our data
         day = line[0:3] # the first three strings of the current line in the loop has been saved to a variable called day, which is the weekday
         if day == "Mon": # this is a conditional that is looking for the strings "Tue", and if that is the case it will do something on the next line
-            print(line) # if the conditional above is met, we print the current line.  We should have output that is all "Tue" in the begininning
+            print(line) # if the conditional above is met, we print the current line.  We should have output that is all "Tue" in the beginning
 
 
-sales_reports(log_file) # this code calls the function
+# sales_reports(log_file) # this code calls the function
+
+
+## Extra Credit ##
+
+def melons(log_file):
+    for line in log_file:
+        line = line.rstrip()
+        if int(line[16:18]) > 10:
+            print(line)
+        
+
+# melons(log_file)
